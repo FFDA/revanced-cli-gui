025da565c7874361f97b03801e20ba8aaef61a26
Domas <nebeklausk@gmail.com>
2024-11-04 16:19:22 +0200

    Add version to title;

--- src/main/java/lt/ffda/revancedcligui/RevancedCliGui.java
+++ src/main/java/lt/ffda/revancedcligui/RevancedCliGui.java
@@ -12,6 +12,8 @@ import lt.ffda.revancedcligui.util.Resource;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class RevancedCliGui extends Application {
     @Override
@@ -19,7 +21,7 @@ public class RevancedCliGui extends Application {
         createFolderStructure();
         FXMLLoader fxmlLoader = new FXMLLoader(RevancedCliGui.class.getResource("view/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 650);
-        stage.setTitle("Revanced-CLI-GUI");
+        stage.setTitle(String.format("Revanced-CLI-GUI v%1$s", getVersion()));
         stage.setScene(scene);
         stage.getIcons().addAll(
                 new Image(RevancedCliGui.class.getResource("icons/icon-16.png").toString()),
@@ -55,5 +57,16 @@ public class RevancedCliGui extends Application {
     public void stop() throws Exception {
         Adb.getInstance().killAdbServer();
         super.stop();
+    }
+
+    private String getVersion() throws IOException {
+        String version = null;
+        Properties properties = new Properties();
+        InputStream input = getClass().getResourceAsStream("/config.properties");
+        if (input != null) {
+            properties.load(input);
+            version = properties.getProperty("version");
+        }
+        return version;
     }
 }
